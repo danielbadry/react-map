a84fc3b5c1edcabd0fe5a2b823599408e2546fbc
Daniel <danielbadry@gmail.com>
2026-03-26 00:20:29 +0100

    feat: add hook to fetch data

---
+++ src/components/map/hooks/use-map.hook.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { fetchMapLocations } from "../api/map.api";
+import type { MapState } from "../types/map.type";
+
+const initialState: MapState = {
+  locations: [],
+  isLoading: true,
+  error: null,
+};
+
+export const useMap = () => {
+  const [state, setState] = useState<MapState>(initialState);
+
+  useEffect(() => {
+    async function loadLocations() {
+      try {
+        const locations = await fetchMapLocations();
+
+        setState({
+          locations,
+          isLoading: false,
+          error: null,
+        });
+      } catch (error) {
+        setState({
+          locations: [],
+          isLoading: false,
+          error:
+            error instanceof Error
+              ? error.message
+              : "Unexpected error while loading the map.",
+        });
+      }
+    }
+
+    void loadLocations();
+  }, []);
+
+  return state;
+};
